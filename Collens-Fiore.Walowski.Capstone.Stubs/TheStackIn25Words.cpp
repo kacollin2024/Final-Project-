a2fe8aae12f0b5a44a903ca79d60554d15319255
Kailey Collins Fiore <kacollin2024@students.curry.edu>
2026-04-29 13:45:44 -0400

    Update TheStackIn25Words.cpp
    Did TheStackIn25Words.cpp file

--- Collens-Fiore.Walowski.Capstone.Stubs/TheStackIn25Words.cpp
+++ Collens-Fiore.Walowski.Capstone.Stubs/TheStackIn25Words.cpp
@@ -1,21 +1,39 @@
 ﻿#include "TheStackIn25Words.h"
 #include <string>
-// #include "Node.h"
+#include "Node.h"
 using namespace std;
 
 TheStackIn25Words::TheStackIn25Words()
 {
     // constructor
+    top = nullptr; 
+    count = 0;
+
 }
 
 TheStackIn25Words::~TheStackIn25Words()
 {
     // deconstructor
     // clears up memory by deleting all nodes
+    while (!stackIsEmpty())
+    {
+        pop();
+    }
 }
 
-void TheStackIn25Words::push()
+void TheStackIn25Words::push(string word)
 {
+    Node* newNode = new Node(word);
+
+    if (stackIsEmpty())
+    {
+        top = newNode;
+    }
+    else
+    {
+        newNode->next = top;
+        top = newNode;
+    }
     // Create new node with this direction
     // Check if our stack is empty (this is the first node)
     // If empty, this node become the top
@@ -25,17 +43,29 @@ void TheStackIn25Words::push()
 
 string TheStackIn25Words::pop()
 {
-    // Check if stack is empty
-    // If empty, tell the user there is nothing to pope and return null
+     // Check if stack is empty
+    if (stackIsEmpty)
+    {
+        // If empty, tell the user there is nothing to pope and return null
+        return "null";`
+    }  
     // Otherwise, grab the top node
-    // move top down to the previous node
+    Node* temp = top;
+    string word = temp->word;
+    top = top->next;
+    delete temp;
+    return word;
 }
 
 bool TheStackIn25Words::stackIsEmpty()
 {
-    // Check if our stack has ny nodes
-    // return true if top is null (empty stack)
+        // return true if top is null (empty stack)
+    if (top ==nullptr)
+    { 
+        return true;
+    }
     // return false if we have nodes (top is not null)
+    return false;
 }
 
 //recycle code from the cave app 
